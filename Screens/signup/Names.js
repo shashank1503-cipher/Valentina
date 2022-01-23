8f32c7a9a3dfa25e8bcfd899c9c7cd9a04d014c9
Akanksha Singh <akanksha.sngh660@gmail.com>
2022-01-23 21:17:49 +0530

    Added form validation

--- Screens/signup/Names.js
+++ Screens/signup/Names.js
@@ -1,32 +1,51 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { View, Text, TextInput } from 'react-native'
 import styles from './Style/Styles'
-import StyledButton from '../../components/Buttons/StyledButton'
-;
-const Names = ({}) => {
-    const [First, setFirst] = useState('')
-    const [Last, setLast] = useState('')
-    const validate=0;
+import StyledButton from '../../components/Buttons/StyledButton';
+import Header from './Header'
+import ErrorMessage from './ErrorMessage'
+import {Formik} from 'formik' 
+import * as Yup from 'yup';
+const Names = () => {
 
+    const validateSchema = Yup.object().shape({
+        first: Yup.string().min(4).max(255).required('First name is required').label("First"),
+        last: Yup.string().max(255).required('Last name is required').label("Last"),
+    }); 
 
     return (
         <View style={ styles.container }>
-            <Text style={styles.heading}>What shall we call you?</Text>
+        <Header title="What shall we call you?"/>
 
-            <TextInput 
-                placeholder="First Name" 
-                style={styles.input}
-                value={First}
-                setValue={setFirst}
-            />
-            <TextInput 
-                placeholder="Last Name" 
-                style={styles.input}
-                value={Last}
-                setValue={setLast}
-            />
+        <Formik
+            initialValues={{first: "", last: ""}}
+            onSubmit={(values) => console.log(values)}
+            validationSchema={validateSchema}
+        >
+            {({handleChange, handleSubmit, errors, values}) => (
+                <React.Fragment>
+                    <TextInput 
+                        placeholder="First Name" 
+                        style={styles.input}
+                        onChangeText={handleChange("first")
+                    }
+                    value={values.first}
+                    />
+                    <ErrorMessage error={errors.first} />
+                    <TextInput 
+                        placeholder="Last Name" 
+                        style={styles.input}
+                        onChangeText={handleChange("last")}
+                        value={values.last}
+                    />
+                    <ErrorMessage error={errors.last} />
 
-            <StyledButton page="Dob" text="Next" onClick={validate}/>
+                    <StyledButton page="Dob" text="Next" validate="{handleSubmit}"/>
+                </React.Fragment>
+            )}
+        
+        </Formik>
+
 
         </View>
     )
