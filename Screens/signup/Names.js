5f23e8f83fa5f25ef86ee0d35b26c698bffcd7c3
Akanksha Singh <akanksha.sngh660@gmail.com>
2022-01-28 22:40:13 +0530

    updated the changes

--- Screens/signup/Names.js
+++ Screens/signup/Names.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Pressable } from 'react-native'
 import styles from './Style/Styles'
-import StyledButton from '../../components/Buttons/StyledButton';
+//import StyledButton from '../../components/Buttons/StyledButton';
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+
 import Header from './Header'
 import ErrorMessage from './ErrorMessage'
 import {Formik} from 'formik' 
 import * as Yup from 'yup';
 const Names = () => {
-
+    const navigation = useNavigation();
+    const colors = ["#FF4E8C", "#FF9B7B", "#F9D7D5"];
+    const Pressed = () => {
+        navigation.navigate("Dob");
+        handleSubmit;
+      }
     const validateSchema = Yup.object().shape({
         first: Yup.string().min(4).max(255).required('First name is required').label("First"),
         last: Yup.string().max(255).required('Last name is required').label("Last"),
@@ -40,7 +48,26 @@ const Names = () => {
                     />
                     <ErrorMessage error={errors.last} />
 
-                    <StyledButton page="Are you old enough?" text="Next" validate="{handleSubmit}"/>
+                    <Pressable  
+                        onPress={() => {
+                            handleSubmit();
+                            navigation.navigate("Are you old enough?");
+                        }}
+                            style={styles.button}
+                    >
+                        <LinearGradient
+                            colors={colors}
+                            style={styles.background}
+                            end={{ x: 0.85, y: 0.15 }}
+                        >
+                         <Text style={styles.text}>
+                            NEXT
+                            </Text>
+                        </LinearGradient>
+                    </Pressable>
+
+                    {/* 
+                    <StyledButton page="Are you old enough?" text="Next" validate="{handleSubmit}"/> */}
                 </React.Fragment>
             )}
         
