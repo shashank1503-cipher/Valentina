bd01d3ca1165346f1cea4a32ab3d7a6a334fdbb1
Akanksha Singh <akanksha.sngh660@gmail.com>
2022-02-01 16:47:59 +0530

    changes

--- Screens/signup/Dob.js
+++ Screens/signup/Dob.js
@@ -1,13 +1,17 @@
 import React, {useState} from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from 'react-native'
 import DatePicker from 'react-native-datepicker';
 import StyledButton from '../../components/Buttons/StyledButton'
 import styles from './Style/Styles'
 import Header from './Header'
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Dob = () => {    
        
-    const [date, setDate] = useState(new Date());  
+    const [date, setDate] = useState(new Date());
+    const navigation = useNavigation();
+    const colors = ["#FF4E8C", "#FF9B7B", "#F9D7D5"];  
          
     return (
         <View style={styles.container}>
@@ -35,7 +39,23 @@ const Dob = () => {
             </TouchableOpacity>  
 
 
-            <StyledButton page="Gender" text="Next"/> 
+            <Pressable  
+              onPress={() => {
+                handleSubmit();
+                navigation.navigate("Gender");
+              }}
+              style={styles.button}
+            >
+              <LinearGradient
+                colors={colors}
+                style={styles.background}
+                end={{ x: 0.85, y: 0.15 }}
+              >
+                  <Text style={styles.text}>
+                    NEXT
+                  </Text>
+              </LinearGradient>
+            </Pressable> 
              
         </View>
     )
