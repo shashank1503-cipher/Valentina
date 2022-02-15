574c4d0ab93b28e0806af9221a25a2c5a965d0ac
Nikhil Menon <74824711+nikhil9302@users.noreply.github.com>
2022-02-15 22:14:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,6 +8,7 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//new keys are regenerated so these keys are not detrimental 
 const firebaseConfig = {
   apiKey: "AIzaSyA1pIrKsikkpiCIG4eEBsfjSI5KQcTjGYQ",
   authDomain: "valentina-834fa.firebaseapp.com",
